7e5e51c55c662a8022d9cd7a26585b5060be9feb
Ambedgar86 <49684285+Ambedgar86@users.noreply.github.com>
2019-04-24 12:04:02 -0700

    Add files via upload

---
+++ MatrixSum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class MatrixSum{
+	public static void main(String args[])
+	{
+		Scanner safe = new Scanner(Syetem.in);
+		int[][] A= new int[3][3];
+		int [][] B= new int[3][3];
+        int [][] sum= new int[3][3];
+        System.out.println("Matrx A ");
+        for(int i=0;i<a.length;i++){
+        	for(j=0;j<B.length;j++){
+        		System.out.println("A("+i+  -j-")");
+        		A[i][j]=safe.nextInt();
+        	}
+        }
+        System.out.println("Matrix B");
+        for(i=0;i<B.length;J++);
+        {
+        	for(j=0;j<B.length;j++){
+        		System.out.println("B("+i+" ; "+j+" ) ");
+        		B[i][j]=safe.nextInt();
+
+        	}
+        }
+        System.out.println("Sum of Matrix A and B");
+        for(int i=0;i<Sum.length;i++){
+        	for(int j=0;j<Sum.length;j++){
+        		Sum[i][j]=A[i][j]+B[i][j];
+        		System.out.println("+i+"  "+j+" ')' "+Sum[i][j]+") ;
+        	}
+        	System.out.println();
+        }
+
+
+	}
+}
